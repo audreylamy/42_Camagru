c31227907c0a5cda8843f93cc15cb21edd9dd44e
Audrey LAMY <alamy@e3r10p14.42.fr>
2018-04-24 19:05:38 +0200

    ajout webcam

--- users.php
+++ users.php
@@ -23,18 +23,99 @@
 			<div id="camera">
 				<div id="part1">
 					<div id="take_picture">
-
+						<button id="activation">Start Camera</button>
+						<div id="camera_space">
+							<video id="video"></video>
+						</div>
+						<div id="button_picture">
+							<button id="startbutton">Take a picture</button>
+						</div>
 					</div>
-					<div id="view_pictures">
+					<div id="choose_object">
 
 					</div>
 				</div>
-				<div id="choose_object">
-						
+				<div id="view_pictures">
+					<canvas id="canvas"></canvas>
 				</div>
 			</div>
 		<footer>
 			<p id="text_footer">Camagru with love</p>
 		</footer>
 	</body>
+	<script language="javascript">
+	var element_start = document.getElementById('activation');
+
+	element_start.addEventListener('click', function()
+	{
+	(function() {
+
+  	var streaming = false,
+      	video        = document.querySelector('#video'),
+      	cover        = document.querySelector('#cover'),
+      	canvas       = document.querySelector('#canvas'),
+      	startbutton  = document.querySelector('#startbutton');
+
+ 	 navigator.getMedia = (navigator.getUserMedia ||
+                    	navigator.webkitGetUserMedia ||
+                        navigator.mozGetUserMedia ||
+                        navigator.msGetUserMedia);
+
+  	navigator.getMedia(
+	{
+     		video: true,
+      		audio: false
+    },
+    function(stream) 
+	{
+      if (navigator.mozGetUserMedia) 
+	  {
+        	video.mozSrcObject = stream;
+      } 
+	  else 
+	  {
+        	var vendorURL = window.URL || window.webkitURL;
+        	video.src = vendorURL.createObjectURL(stream);
+      }
+      video.play();
+    },
+    function(err) 
+	{
+      	console.log("An error occured! " + err);
+    }
+  );
+
+  video.addEventListener('canplay', function(ev){
+    if (!streaming) 
+	{
+		var width_camera = 810;
+      	height = video.videoHeight / (video.videoWidth/width_camera);
+      	video.setAttribute('width', width_camera);
+      	video.setAttribute('height', height);
+      	canvas.setAttribute('width',width_camera);
+      	canvas.setAttribute('height', height);
+      	streaming = true;
+    }
+  }, false);
+
+  function takepicture() 
+  {
+	  	var height_picture = 300;
+	  	var width_picture = 420;
+    	canvas.width = width_picture;
+    	canvas.height = height_picture;
+    	canvas.getContext('2d').drawImage(video, 0, 0, width_picture, height_picture);
+    	var data = canvas.toDataURL('uploads');
+    	photo.setAttribute('src', data);
+  }
+
+  startbutton.addEventListener('click', function(ev)
+  {
+      	takepicture();
+    	ev.preventDefault();
+  }, false);
+
+})();
+});
+	</script>
 </html>
